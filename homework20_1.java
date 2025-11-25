e6d0474ee1815a59c5e806e8b970492dcbd048fd
ArielMyersWcupa <AM1044820@wcupa.edu>
2025-11-25 17:39:08 -0500

    Update homework20_1.java

--- homework20_1.java
+++ homework20_1.java
@@ -24,27 +24,29 @@ public class homework20_1
 {
    public static void main(String[] args)
    {
-     Scanner in = new Scanner(System.in);
-      //Read the five numbers and build the linked list//
-      LinkedList List = new LinkedList();
-      if (in.hasNextInt()) {
-         list.head = new ListNode(in.NextInt()); //First node turns into the head//
-         ListNode tail = list.head;
+      Scanner in = new Scanner(System.in);
 
-         //Add the next four numbers eo the end of the list//
-         for (int i = 1; i < 5; i++) {
-            if (in.hasNextInt()) {
-               int v = in.nextInt();
-               tail.next = new ListNode(v);
-               tail = tail.next;
-               
-            }
-         }
+      //Create an empty list//
+      LinkedList list = new LinkedList();
+
+      //Read 5 numbers and put them into the linked list//
+      //First number becomes the head//
+      list.head = new ListNode(in.nextInt());
+      ListNode tail = list.head;
+
+      //Add in the next 4 numbers//
+      for (int i = 1; i < 5; i++)
+      {
+         int v = in.nextInt(); 
+         tail.next = new ListNode(v); 
+         tail = tail.next;   //Move the tail forward//
       }
-      
-      deleteDuplicates(list); //Delete duplicates from the list//
-      
-      System.out.println(list.toString()); //Prints out the result//
+
+      //Deletes the duplicates//
+      deleteDuplicates(list);
+
+      //Prints the result//
+      System.out.println(list.toString());
 
       in.close();
    }
@@ -64,7 +66,6 @@ public class homework20_1
          }
       }
    } 
-
 }
 
 class ListNode
